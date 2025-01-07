18a3a74411d9d90eb475c31c183a90c4014487d6
Ingrid Ornum <ingrid.ornum@gmail.com>
2025-01-07 14:45:49 +0100

    Chore: finished tasks

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,11 @@
 //takes in a word and returns true if the word is a palindrome, false if it is not
-export function isWordPalindrome(str) {}
+export function isWordPalindrome(str) {
+    const formattedStr = str.toLowerCase();
+    return formattedStr === formattedStr.split('').reverse().join('');
+}
+
 //takes in a sentence and returns true if the sentence is a palindrome, false if it is not
-export function isSentencePalindrome(str) {}
+export function isSentencePalindrome(str) {
+    const formattedStr = str.toLowerCase().replace(/[^a-z]/g, '');
+    return formattedStr === formattedStr.split('').reverse().join('');
+}
